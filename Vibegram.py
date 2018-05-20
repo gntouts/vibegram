83e8f450ffd3bcd2945d0bb4283c03e3771d3e8e
George_Tasop <39223003+GeorgeTasop@users.noreply.github.com>
2018-05-20 16:22:09 +0300

    Update Vibegram.py

--- Vibegram.py
+++ Vibegram.py
@@ -1,5 +1,5 @@
 import sqlite3
-import datetime
+import datetime as dt
 from dbclass import *
 from DBHandling import *
 
@@ -16,21 +16,55 @@ def WelcomeMenu():
 
 def WelcomeAction(choice, c):
 
-    if (choice==1):
+    if (choice == 1):
         found = 0
-        while (found):
-            email = input("Email: ")##In create_user function we should run a test to make sure the e-mail isnt already in use
-            password = input("Password: ")##giati trexeis to search edw, de tha mpei se ksexwristo function?
+        while (not found):
+            print ("----Sign In----")
+            print ("Email: ", end='')
+            email = input()##In create_user function we should run a test to make sure the e-mail isnt already in use
+            print ("Password: ", end='')
+            password = input()##giati trexeis to search edw, de tha mpei se ksexwristo function?
             cur = c.execute("SELECT user_id FROM Users WHERE e_mail = :email AND password = :password", {"email":email, "password":password})
             for r in cur:
                 found = r[0]
             if (not found):
-                print ("Wrong username or password, try again!")
+                print ("Wrong username or password, try again!\n")
+        return found
+    if (choice == 2):
+        print ("----Sign Up----")
+        print ("Name: ", end='')
+        name = input()
+        print ("Last Name: ", end='')
+        lastname = input()
+        print ("Email: ", end='')
+        email = input()
+        print ("Phone: ", end='')
+        phone = input()
+        print ("Password: ", end='')
+        password = input()
+        print ("Avatar: ", end='')
+        avatar = input()
 
+        print ("Creating account...")
+        user = Users('', name, lastname, avatar, phone, \
+                     email, password, dt.datetime.now() , 'y')
 
-def MainMenu():
-    pass
+        ins_user(user)
             
+def MainMenu(userid, c):
+    cur = c.execute("SELECT first_name, last_name FROM Users WHERE user_id = ?", (userid,))
+    for r in cur:
+        name = r[0]
+        lastname = r[1]
+    print ("Hello", name, lastname, "!")
+
+    #gia pes gnwmh gia edw...mallon den ftanei ena kentriko interface
+    print ("Choose one of the following:")
+    print ("1) See contacts")
+    print ("2) Create message")
+    print ("0) Exit")
+
+        
 def main():
     conn = sqlite3.connect('vibegram.db')
     c = conn.cursor()
@@ -41,10 +75,11 @@ def main():
     print ("Done!")
 
     choice = WelcomeMenu()
-    WelcomeAction(choice, c)
-    print("Welcome to Vibegram")
-    choice = MainMenu()
-    
+    userid = WelcomeAction(choice, c)
+    choice = MainMenu(userid, c)
 
-if __name__ = "__main__":
+    print ("Closing...")
+    conn.commit()
+    conn.close
+if __name__ == "__main__":
     main()
