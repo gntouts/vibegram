460267cc23de033613bce6adb328c336c4dbcf01
George_Tasop <39223003+GeorgeTasop@users.noreply.github.com>
2018-05-20 20:55:49 +0300

    Update UserFunctions.py

--- UserFunctions.py
+++ UserFunctions.py
@@ -1,15 +1,15 @@
 import sqlite3
-import datetime
+import datetime as dt
 from dbclass import *
 from DBHandling import *
 
 def Check_if_exists(user_mail):
-	id = search_by_email(user_mail)
-	##if....
+    id = search_by_email(user_mail)
+    ##if....
 
 
-def Create_Acc(c):##
-	##allows user to create an new acc and 
+def Create_Acc(c):
+    ##allows user to create an new acc and 
     ##insert name, avatar, phone, email and password
     print ("----Sign Up----")
     print ("Name: ", end='')
@@ -37,8 +37,8 @@ def Create_Acc(c):##
 
 
 def Sign_In(c):
-	##allows user to sign in using his email and password
-    	##returns his user_id
+    ##allows user to sign in using his email and password
+    ##returns his user_id
     ##changes users 'is_active' parameter
     found = 0
     while (not found):
@@ -55,7 +55,7 @@ def Sign_In(c):
     return found
 	
 def Update_Account(userid, c):
-	##allows end user to change name, avatar, phone, email and password
+    ##allows end user to change name, avatar, phone, email and password
     a = -1
     while (a not in range(7)):
         print ("Choose what to change: ")
@@ -108,81 +108,125 @@ def Update_Account(userid, c):
 
         if (a):
               print ("Profile updated successfully!")
+              
+def Search_Users(c):
+    results = ['0']
+    print ("Search: ", end='')
+    words = input()
+    search_first(words, results)
+    for i in range(1, len (results)):
+        print (i, end=' ')
+        print_usnames(results[i])
+    return results
+    
+def Add_Friend(a, userid1,userid2, c, friendtype):
+    ##allows user to search and befriend another user
+    ##searches by name/phone/email/user_id
+    if (a):
+        c.execute("""INSERT INTO Friend (friend1_id, friend2_id,
+                      date_added, friend_type)
+                      VALUES (:friend1_id, :friend2_id,
+                      :date_added, :friend_type)""",
+                      {"friend1_id" : userid1, "friend2_id" : userid2,
+                      "date_added" : dt.datetime.now(), "friend_type" : friendtype})
+	
+def Contact_List(userid, c):
+    ##returns users contact list grouped by friend_type
+    ##also shows if contacts are active
+    #Users.first_name, Users.last_name, Users.is_active
+    #JOIN Users ON Friend.friend2_id = Users.user_id)
+    cur = c.execute("""SELECT DISTINCT U.user_id, U.first_name, U.last_name, U.is_active,
+                    Users.user_id, Users.first_name, Users.last_name, Users.is_active
+                    FROM ( (Users as U) JOIN
+                    (Friend JOIN Users ON Friend.friend1_id = Users.user_id)
+                    ON U.user_id = Friend.friend2_id)
+                    WHERE Friend.friend2_id = :userid OR Friend.friend1_id = :userid;""",
+                    {"userid" : userid})
+    print ("\n----Friends-----")
+    for r in cur:
+        #elegxos wste na mhn ektypwnei to onoma sou alla to onoma tou filou sou
+        if (r[0] == userid):
+            #elegxos ama o filos einai active
+            if (r[3] == 'y'):
+                print (r[5], r[6], "Active")
+            else:
+                print (r[5], r[6], "Inactive") 
+        else:
+            
+            if (r[2] == 'y'):
+                print (r[1], r[2], "Active")
+            else:
+                print (r[1], r[2], "Inactive")
+                
+def Create_Mess(userid, userid2, c):
+    ##allows user to create new message and send it or save it as draft
+    print ("Type here: ", end='')
+    body = input()
+    mess = Messages('', userid, 'n', dt.datetime.now(), body, '')
+    create_message(mess)
 
-def Add_Friend():
-	##allows user to search and befriend another user
-	##searches by name/phone/email/user_id
-	pass
-	
-def Contact_List():
-	##returns users contact list grouped by friend_type
-	##also shows if contacts are active
-	pass
-	
-def Create_Mess():
-	##allows user to create new message and send it or save it as draft
-	pass
+    #kai edw kati prepei na ginetai kai na apothikeuetai to mnm sto pinaka me tous recepients
 	
 def Send_Mess():
-	##allows user to send message to One, Many or Group
-	pass
+    ##allows user to send message to One, Many or Group
+    pass
 	
 def See_Conv():
-	##allows user to see a conversation with a friend
-	##also shows if past messages are delivered 
-	##and their timestamp of delivery
-	##makes all messages received by that user is_received
-	pass
+    ##allows user to see a conversation with a friend
+    ##also shows if past messages are delivered 
+    ##and their timestamp of delivery
+    ##makes all messages received by that user is_received
+    pass
 	
 def Mess_Delivered():
-	##absolutely no clue how to implement this
-	pass
+    ##absolutely no clue how to implement this
+    pass
 	
 def Delete_Friend(user_id, friend_id):
-	##allows user to delete friend
-	with conn:
-		c.execute("""DELETE from Friend
-					WHERE (friend1_id = :user_id AND friend2_id = :friend_id)
-					OR (friend2_id = :user_id AND friend1_id = :friend_id)""",
-					{'user_id': user_id, 'friend_id': friend2_id}
-					)
+    ##allows user to delete friend
+    with conn:
+            c.execute("""DELETE from Friend
+                                    WHERE (friend1_id = :user_id AND friend2_id = :friend_id)
+                                    OR (friend2_id = :user_id AND friend1_id = :friend_id)""",
+                                    {'user_id': user_id, 'friend_id': friend2_id}
+                                    )
 	
 def Change_friend_type(user_id, friend_id, new_type):
-	##allows user to change friendship type
-	with conn:
-		c.execute("""UPDATE Users
-					SET friend_type = :friend_tpe
-					WHERE (friend1_id = :user_id 
-					AND friend2_id = :friend_id)
-					OR (friend2_id = :user_id 
-					AND friend1_id = :friend_id)
-					""",
-					{'friend_type': new_type, 'friend1_id': name, 'friend2_id': lastname}
-					)
-	
+    ##allows user to change friendship type
+    with conn:
+            c.execute("""UPDATE Users
+                                    SET friend_type = :friend_tpe
+                                    WHERE (friend1_id = :user_id 
+                                    AND friend2_id = :friend_id)
+                                    OR (friend2_id = :user_id 
+                                    AND friend1_id = :friend_id)
+                                    """,
+                                    {'friend_type': new_type, 'friend1_id': name, 'friend2_id': lastname}
+                                    )
+    
 def Delete_Account(user_id):
-	##allows user to delete account
-	##this deletes all his friendships
-	##and changes all the messages sent or received by him
-	##to sent/received by unknown
-	with conn:
-		deleted_id = 'UNKNOWN'
-		
-		c.execute("DELETE from Users WHERE user_id = :user_id",
-                  {'user_id': user_id})
-		c.execute("""DELETE from Friend
-					WHERE friend1_id = :user_id OR friend2_id = :user_id""",
-					{'user_id': user_id}
-					)
-		c.execute("""UPDATE Messages
-					SET sender_id = :deleted_id
-					WHERE sender_id = :user_id""",
-					{'deleted_id': deleted_id, 'user_id': user_id}
-					)
-		c.execute("""UPDATE Receiving
-					SET recipient_id = :deleted_id
-					WHERE recipient_id = :user_id""",
-					{'deleted_id': deleted_id, 'user_id': user_id}
-					)		
-					
-					
+    ##allows user to delete account
+    ##this deletes all his friendships
+    ##and changes all the messages sent or received by him
+    ##to sent/received by unknown
+    with conn:
+            deleted_id = 'UNKNOWN'
+            
+            c.execute("DELETE from Users WHERE user_id = :user_id",
+              {'user_id': user_id})
+            c.execute("""DELETE from Friend
+                                    WHERE friend1_id = :user_id OR friend2_id = :user_id""",
+                                    {'user_id': user_id}
+                                    )
+            c.execute("""UPDATE Messages
+                                    SET sender_id = :deleted_id
+                                    WHERE sender_id = :user_id""",
+                                    {'deleted_id': deleted_id, 'user_id': user_id}
+                                    )
+            c.execute("""UPDATE Receiving
+                                    SET recipient_id = :deleted_id
+                                    WHERE recipient_id = :user_id""",
+                                    {'deleted_id': deleted_id, 'user_id': user_id}
+                                    )		
+                                    
+                                    
