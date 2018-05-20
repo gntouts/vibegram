2fa28722a0bdd5bef0de3091df5113341f338a3e
krinUF <33500389+krinUF@users.noreply.github.com>
2018-05-20 14:22:01 +0300

    Update UserFunctions.py

--- UserFunctions.py
+++ UserFunctions.py
@@ -7,6 +7,12 @@ def Create_Acc():
 	##allows user to create an new acc and 
 	##insert name, avatar, phone, email and password
 	pass
+
+def Sign_In():
+	##allows user to sign in using his email and password
+	##returns his user_id
+	##changes users 'is_active' parameter
+	pass
 	
 def Update_Account():
 	##allows end user to change name, avatar, phone, email and password
@@ -15,6 +21,11 @@ def Update_Account():
 def Add_Friend():
 	##allows user to search and befriend another user
 	##searches by name/phone/email/user_id
+	pass
+	
+def Contact_List():
+	##returns users contact list grouped by friend_type
+	##also shows if contacts are active
 	pass
 	
 
