f42a455e57853eeb298d13f01efddc5cefb37af1
krinUF <33500389+krinUF@users.noreply.github.com>
2018-05-20 14:17:15 +0300

    Add files via upload

---
+++ UserFunctions.py
@@ -0,0 +1,21 @@
+import sqlite3
+import datetime
+from dbclass import *
+from DBHandling import *
+
+def Create_Acc():
+	##allows user to create an new acc and 
+	##insert name, avatar, phone, email and password
+	pass
+	
+def Update_Account():
+	##allows end user to change name, avatar, phone, email and password
+	pass
+
+def Add_Friend():
+	##allows user to search and befriend another user
+	##searches by name/phone/email/user_id
+	pass
+	
+
+	
