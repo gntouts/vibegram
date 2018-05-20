b2db77816e10ce3b94a659dc6e12ffeda2187b4b
George_Tasop <39223003+GeorgeTasop@users.noreply.github.com>
2018-05-20 15:34:13 +0300

    Update Vibegram.py

--- Vibegram.py
+++ Vibegram.py
@@ -3,4 +3,48 @@ import datetime
 from dbclass import *
 from DBHandling import *
 
-##where the magic happens
+
+def WelcomeMenu():
+    a = -1
+    while (a not in range(3)):
+        print ("Choose one of the following:")
+        print ("1) Sign In")
+        print ("2) Sign Up")
+        print ("0) Exit program")
+        a = int(input())
+    return a
+
+def WelcomeAction(choice, c):
+
+    if (choice==1):
+        found = 0
+        while (found):
+            email = input("Email: ")
+            password = input("Password: ")
+            cur = c.execute("SELECT user_id FROM Users WHERE e_mail = :email AND password = :password", {"email":email, "password":password})
+            for r in cur:
+                found = r[0]
+            if (not found):
+                print ("Wrong username or password, try again!")
+
+
+def MainMenu():
+    pass
+            
+def main():
+    conn = sqlite3.connect('vibegram.db')
+    c = conn.cursor()
+
+    #gia arxh meta tha to bgaloume
+    print ("Creating database...")
+    CreateTables(c)
+    print ("Done!")
+
+    choice = WelcomeMenu()
+    WelcomeAction(choice, c)
+    print("Welcome to Vibegram")
+    choice = MainMenu()
+    
+
+if __name__ = "__main__":
+    main()
