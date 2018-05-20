d17a05cc1fa49a312ddc503e5bb8c968fc92860a
George_Tasop <39223003+GeorgeTasop@users.noreply.github.com>
2018-05-20 17:32:38 +0300

    Update UserFunctions.py

--- UserFunctions.py
+++ UserFunctions.py
@@ -8,20 +8,106 @@ def Check_if_exists(user_mail):
 	##if....
 
 
-def Create_Acc():##
+def Create_Acc(c):##
 	##allows user to create an new acc and 
-	##insert name, avatar, phone, email and password
-	pass
+    ##insert name, avatar, phone, email and password
+    print ("----Sign Up----")
+    print ("Name: ", end='')
+    name = input()
+    print ("Last Name: ", end='')
+    lastname = input()
+    print ("Email: ", end='')
+    email = input()
+    print ("Phone: ", end='')
+    phone = input()
+    print ("Password: ", end='')
+    password = input()
+    print ("Avatar: ", end='')
+    avatar = input()
 
-def Sign_In():
+    print ("Creating account...")
+    user = Users('', name, lastname, avatar, phone, \
+                 email, password, dt.datetime.now() , 'y')
+
+    ins_user(user)
+    cur = c.execute("SELECT user_id FROM Users WHERE e_mail = :email AND password = :password", {"email":email, "password":password})
+    for r in cur:
+        uid = r[0]
+    return uid
+
+
+def Sign_In(c):
 	##allows user to sign in using his email and password
-	##returns his user_id
-	##changes users 'is_active' parameter
-	pass
+    	##returns his user_id
+    ##changes users 'is_active' parameter
+    found = 0
+    while (not found):
+        print ("----Sign In----")
+        print ("Email: ", end='')
+        email = input()
+        print ("Password: ", end='')
+        password = input()
+        cur = c.execute("SELECT user_id FROM Users WHERE e_mail = :email AND password = :password", {"email":email, "password":password})
+        for r in cur:
+            found = r[0]
+        if (not found):
+            print ("Wrong username or password, try again!\n")
+    return found
 	
-def Update_Account():
+def Update_Account(userid, c):
 	##allows end user to change name, avatar, phone, email and password
-	pass
+    a = -1
+    while (a not in range(7)):
+        print ("Choose what to change: ")
+        print ("1) Email")
+        print ("2) Password")
+        print ("3) Name")
+        print ("4) Last Name")
+        print ("5) Avatar")
+        print ("6) Phone")
+        print ("0) Go Back")
+        a = int(input())
+        if (a==1):
+            print ("Email: ", end='')
+            email = input()
+            c.execute("""UPDATE Users SET e_mail = :email
+                        WHERE user_id = :userid""", \
+                      {"email" : email, "userid" : userid})
+        elif (a==2):
+            print ("Password: ", end='')
+            password = input()
+            c.execute("""UPDATE Users SET password = :password
+                        WHERE user_id = :userid""", \
+                      {"password" : password, "userid" : userid})
+        elif (a==3):
+            print ("Name: ", end='')
+            name = input()
+            c.execute("""UPDATE Users SET first_name = :name
+                        WHERE user_id = :userid""", \
+                      {"name" : name, "userid" : userid})
+        elif (a==4):
+            print ("Last Name: ", end='')
+            lastname = input()
+            c.execute("""UPDATE Users SET last_name = :lastname
+                        WHERE user_id = :userid""", \
+                      {"lastname" : lastname, "userid" : userid})
+        elif (a==5):
+            print ("Avatar: ", end='')
+            avatar = input()
+            c.execute("""UPDATE Users SET avatar = :avatar
+                        WHERE user_id = :userid""", \
+                      {"avatar" : avatar, "userid" : userid})
+        elif (a==6):
+            print ("Phone: ", end='')
+            phone = input()
+            c.execute("""UPDATE Users SET phone_number = :phone
+                        WHERE user_id = :userid""", \
+                      {"phone" : phone, "userid" : userid})
+        else:
+              pass
+
+        if (a):
+              print ("Profile updated successfully!")
 
 def Add_Friend():
 	##allows user to search and befriend another user
