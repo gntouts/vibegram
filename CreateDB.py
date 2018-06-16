146af7b75847e9ec060004c635346626fcb715ca
George_Tasop <39223003+GeorgeTasop@users.noreply.github.com>
2018-06-16 20:15:55 +0300

    Update CreateDB.py

--- CreateDB.py
+++ CreateDB.py
@@ -5,10 +5,9 @@ from DML import *
 
 def CreateTables():
 
-    conn = sqlite3.connect('vibegram.db')
+    conn = sqlite3.connect('vibegramDB.db')
     c = conn.cursor()
     
-    #Allagh apo Users se User
     c.execute("""CREATE TABLE IF NOT EXISTS User (
                     user_id integer PRIMARY KEY AUTOINCREMENT,
                     first_name varchar,
@@ -30,7 +29,7 @@ def CreateTables():
                     friend_type varchar
                     );
                     """)
-    #Allagh apo Messages se Message                
+					
     c.execute("""CREATE TABLE IF NOT EXISTS Message (
                          message_id integer PRIMARY KEY AUTOINCREMENT,
                          sender_id integer,
@@ -64,16 +63,24 @@ def PopulateDB():
 
     #------------------Create Users-------------------
     
-    for i in range(1000):
+    for i in range(100):
         #---------------Create dummy data---------------
         first_name = names.get_first_name()
         last_name = names.get_last_name()
         email = first_name[0]+"."+last_name+str(random.randint(1, 99))+"@gmail.com"
         phone = int("69" + str(random.randint(10000000, 99999999)))
 
+
+        #------------Password hashing---------------
+            
+        raw_password = "pass"
+        salt = hashlib.sha256(str(random.getrandbits(512)).encode('utf-8')).hexdigest()[:5]
+        hsh = hashlib.sha256(('%s%s' % (salt, raw_password)).encode('utf-8')).hexdigest()
+        final_password = '%s$%s' % (salt, hsh)
+        
         #---------------Insert User--------------------------
         Ins_User(first_name, last_name, "", phone, \
-                 email, "pass", dt.datetime.now(), 'n' )
+                 email, final_password, dt.datetime.now(), 'n' )
     
     
                         
